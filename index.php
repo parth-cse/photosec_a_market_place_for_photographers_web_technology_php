df162634dd6fad36777d1574a38339b62a921461
Parth <ppj8988@gmail.com>
2024-04-17 00:58:18 +0530

    Add Hero Section in index.php

--- index.php
+++ index.php
@@ -2,7 +2,18 @@
     include('./includes/header.php');
 ?>
 
-<p>Indexx</p>
+<div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="./static/img/site/hero.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">We are a Market Place for Photographers. A Platform to Showcase the Photography Talent</h1>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+        </div>
+      </div>
+    </div>
+  </div>
 
 <?php
     include('./includes/footer.php');
