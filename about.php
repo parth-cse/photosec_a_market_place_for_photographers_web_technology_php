7ac92e9d6c0a715564949a3e531bda326bc4282c
Parth <ppj8988@gmail.com>
2024-04-20 01:09:05 +0530

    About us Complete No Need to Touch it

--- about.php
+++ about.php
@@ -1,15 +1,28 @@
 <?php
     include('./includes/header.php');
 ?>
-<h1>About Us</h1>
-<div>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit reiciendis molestiae voluptatum pariatur iusto qui, similique vitae nam non rem amet a harum perferendis tempore?</p>
-    <img src="./static/img/site/about-img.jpg" alt="">
-</div>
-<div>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit reiciendis molestiae voluptatum pariatur iusto qui, similique vitae nam non rem amet a harum perferendis tempore?</p>
-    <img src="./static/img/site/about-img.jpg" alt="">
-</div>
+<section id="about">
+            <div class="heading">
+                <h2 style="color: #F4E079;" class="section-heading">About us</h2>
+            </div>
+            <div class="about" style="border-top-width: 2px;">
+                <div class="about-content">
+                <div><h1 style="text-align: justify;">We are passionate about the power of photography to capture life's special moments and tell visual stories. We believe everyone deserves access to talented photographers who can bring their vision to life, whether it's a wedding, a family portrait, a product launch, or anything in between.</h1></div>
+                <div><img src="./static/img/site/about-img.jpg" alt="About Image" ></div>
+                </div>
+            </div>
+            <div class="about">
+                <div class="about-content">
+                <div><img src="./static/img/site/about-img2.jpg" alt="About Image" ></div>
+                <div><h1 style="text-align: justify;">Our mission is to create a thriving marketplace that empowers photographers and simplifies the booking process for clients. We aim to:
+                    <ul><br>
+                    <li>Provide photographers with a platform to showcase their work, connect with new clients, and manage their bookings efficiently.</li><br>
+                    <li>Offer clients a wide range of talented photographers to choose from, with a user-friendly search and booking system.</li><br>
+                    <li>Foster a vibrant community where photographers can network, share their expertise, and stay up-to-date on industry trends.</li><br>
+                    </ul></h1></div>
+                </div>
+            </div>
+        </section>
 <?php
     include('./includes/footer.php');
 ?>
