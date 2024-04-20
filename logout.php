a54ba1c57315d3ec197e996a577e43c17216cef2
Parth <ppj8988@gmail.com>
2024-04-20 20:38:00 +0530

    Front end design complete user dashboard

--- logout.php
+++ logout.php
@@ -1,8 +1,13 @@
 <?php
 include('includes/config.php');
-session_start();
+
 
 if($_SERVER['REQUEST_METHOD']=='POST'){
+    session_unset();
+    header('Location:login.php');
+    $_SESSION['loggedin'] = false;
+}
+else{
     session_unset();
     header('Location:login.php');
     $_SESSION['loggedin'] = false;
