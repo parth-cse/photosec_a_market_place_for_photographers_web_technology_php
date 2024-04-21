b49ba32a4a54dcc255b05ee03a854fdd34e6b8f0
Parth <ppj8988@gmail.com>
2024-04-22 03:37:40 +0530

    Your Booking Complete

--- yourbookings.php
+++ yourbookings.php
@@ -5,62 +5,50 @@
     
     include('./includes/header.php');
     if($_SESSION['loggedin'] === true){
-
+        $email = $_SESSION['email'];
     }else{
         echo "<script>alert('Please log in first'); window.location= 'login.php';</script>"; 
     }
+    $sql = "SELECT * FROM bookings WHERE clientEmail='$email'";
+    $result = $conn->query($sql);
     
 ?>
 <section id="bookings">
+    <?php if($result->num_rows>0) { while($row = $result-> fetch_assoc()){ ?>
             <div class="booking">
                 <div class="booking-details">
                     
                     <div class="details">
                         <div>
-                            <p class="booking-id">Booking Id: XXX</p>
-                            <div><p>Booking for: YYYY/MM/DD</p><br>
-                            <p>No of Days: aa</p><br>
-                            <p>Name of Photographer: abc</p><br>
-                            <p>Booking Status: Accepted</p></div><br>
+                            <p class="booking-id">Booking Id: <?php echo "".$row['id']; ?></p>
+                            <div><p>Booking for: <?php echo "".$row['bookingDate']; ?></p><br>
+                            <p>No of Days: <?php echo "".$row['numberOfDays']; ?></p><br>
+                            <?php $pgemail = $row["photoGrapherEmail"];
+                            $pgnameSQL = "SELECT * FROM photographeruser WHERE email='$pgemail'";
+                            $pgresult = $conn -> query( $pgnameSQL );
+                            $pgrow = $pgresult -> fetch_assoc();
+                            ?>
+                            <p>Name of Photographer: <?php echo "".$pgrow['name']; ?></p><br>
+                            <p>Booking Status: <span style="color:#F4E079"><?php echo"".$row['status']; ?></span></p></div><br>
                         </div>
                         <div  class="bookedPhotographer">
-                            <p class="booking-date">Date: YYYY/MM/DD</p>
-                            <a href=""><img src="./static/img/site/sanjay.jpg" alt=""></a>
+                            <p class="booking-date">Date: <?php echo "".$row['bookAt'] ?></p>
+                            <a href="./photographer-porfile.php?slug=<?php echo"".$pgrow['email'] ?>"><img src="<?php echo "".$pgrow['profile'] ?>" alt=""></a>
                         </div>
                     </div>
                     <div class="booking-btn">
-                        <a href="" class="btn-secondary">Aproved</a>
-                        <form action="" method="post">
-                            <button type="submit" class="btn-primary red-btn btn-border">Cancel</button>
+                        <div class="btn-primary"><?php echo"".$row['status']; ?></div>
+                        <form action="cancelbooking.php">
+                            <button type="submit" name='bookingid' value="<?php echo "".$row['id'] ?>" class="btn-primary red-btn btn-border">Cancel</button>
                         </form>
                     </div>
                 </div>
             </div>
-            <hr />
-            <div class="booking">
-                <div class="booking-details">
-                    
-                    <div class="details">
-                        <div>
-                            <p class="booking-id">Booking Id: XXX</p>
-                            <div><p>Booking for: YYYY/MM/DD</p><br>
-                            <p>No of Days: aa</p><br>
-                            <p>Name of Photographer: abc</p><br>
-                            <p>Booking Status: Accepted</p></div><br>
-                        </div>
-                        <div  class="bookedPhotographer">
-                            <p class="booking-date">Date: YYYY/MM/DD</p>
-                            <a href=""><img src="./static/img/site/sanjay.jpg" alt=""></a>
-                        </div>
-                    </div>
-                    <div class="booking-btn">
-                        <a href="" class="btn-secondary">Aproved</a>
-                        <form action="" method="post">
-                            <button type="submit" class="btn-primary red-btn btn-border">Cancel</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+            <hr /><?php  }} else { ?>
+                <div class="photographer-name">
+                                <h2 style="text-align: center; margin-top: 50px">Make your First Booking Requests <a href="photographer.php">click here</a></h2>
+                            </div> <?php } ?>
+            
         </section>
 <?php
     include('./includes/footer.php');
